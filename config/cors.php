6a9fd4244e587324585af40975652b51800ef9dc
Khairulbashar010 <khairul.bashar.rpsu@gmail.com>
2025-07-02 21:59:36 +0800

    update cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
     
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:8001'],
+    'allowed_origins' => ['*', 'http://localhost:8001', 'http://121.0.01:8001'],
 
     'allowed_origins_patterns' => [],
 
